58f3f7f3d2ebfebb2fd8fd7c540bf223b5fb5a3e
JDarron <j.dspears@yahoo.com>
2021-12-03 09:57:26 -0800

    made some changez

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -1,12 +1,12 @@
-$(function () {
+$(function() {
 
 	$("input,textarea").jqBootstrapValidation(
 		{
 			preventSubmit: true,
-			submitError: function ($form, event, errors) {
+			submitError: function($form, event, errors) {
 				// something to have when submit produces an error ?
 			},
-			submitSuccess: function ($form, event) {
+			submitSuccess: function($form, event) {
 				event.preventDefault(); // prevent default submit behaviour
 				// get values from FORM
 				var name = $("input#name").val();
@@ -18,14 +18,15 @@ $(function () {
 					firstName = name.split(' ').slice(0, -1).join(' ');
 				}
 				$.ajax({
-					url: "/email",
+					url: "/assets/mail/contact_me.php",
 					type: "POST",
-					data: { name: name, email: email, message: message },
-					success: function () {
+					data: { name,  email,  message },
+					cache: false,
+					success: function() {
 						// Success message
 						$('#success').html("<div class='alert alert-success'>");
 						$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-							.append("</button>");
+							.append( "</button>");
 						$('#success > .alert-success')
 							.append("<strong>Your message has been sent. </strong>");
 						$('#success > .alert-success')
@@ -34,24 +35,24 @@ $(function () {
 						//clear all fields
 						$('#contactForm').trigger("reset");
 					},
-					error: function () {
+					error: function() {
 						// Fail message
 						$('#success').html("<div class='alert alert-danger'>");
 						$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-							.append("</button>");
-						$('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:j.dspears@yahoo.com?Subject=Message_Me from myprogrammingblog.com'>j.dspears@yahoo.com</a> ? Sorry for the inconvenience!");
+							.append( "</button>");
+						$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:phillips@joshuadarron.com?Subject=Message_Me from myprogrammingblog.com'>phillips@joshuadarron.com</a> ? Sorry for the inconvenience!");
 						$('#success > .alert-danger').append('</div>');
 						//clear all fields
 						$('#contactForm').trigger("reset");
 					},
 				})
 			},
-			filter: function () {
+			filter: function() {
 				return $(this).is(":visible");
 			},
 		});
 
-	$("a[data-toggle=\"tab\"]").click(function (e) {
+	$("a[data-toggle=\"tab\"]").click(function(e) {
 		e.preventDefault();
 		$(this).tab("show");
 	});
@@ -59,6 +60,6 @@ $(function () {
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function () {
+$('#name').focus(function() {
 	$('#success').html('');
 });
